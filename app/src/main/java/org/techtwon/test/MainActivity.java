ab1b0ac6aeb890e172e8413da2afc7f971626fd0
CYE <uniye21@gmail.com>
2022-05-11 19:53:33 +0900

    .

--- app/src/main/java/org/techtwon/test/MainActivity.java
+++ app/src/main/java/org/techtwon/test/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.SpannableString;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,12 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
         Button user_login_button = findViewById(R.id.login_by_user);
         Button vet_login_button = findViewById(R.id.login_by_vet);
-
-        String content = user_login_button.getText().toString();
-        SpannableString spannableString = new SpannableString(content);
-        String word = "사용자";
-        int start = content.indexOf(word);
-        int end = start + word.length();
 
         user_login_button.setOnClickListener(new View.OnClickListener() {
             @Override
