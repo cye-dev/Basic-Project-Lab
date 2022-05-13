0bbe857e0503ca8024975e041bc22f2cd768f3c8
YE <uniye21@gmail.com>
2022-05-13 14:34:54 +0900

    file name fixed (Main -> LogIn Selection)

--- app/src/main/java/org/techtwon/test/LogInSelection.java
+++ app/src/main/java/org/techtwon/test/LogInSelection.java
@@ -7,11 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LogInSelection extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_selection);
 
         Button user_login_button = findViewById(R.id.login_by_user);
         Button vet_login_button = findViewById(R.id.login_by_vet);
